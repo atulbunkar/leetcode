dea004dedd3d590c0fa040958513c975ca217de9
Atul Bunkar <atul.bunkar04@yahoo.com>
2021-12-15 22:20:07 +0530

    Time: 0 ms (100.00%), Space: 8 MB (36.45%) - LeetHub

---
+++ house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        if(nums.empty()) return 0;
+        if(nums.size() == 1) return nums[0];
+        
+        vector<int> dp(nums.begin(), nums.end()-1);
+        vector<int> dp2(nums.begin()+1, nums.end());
+        
+        return max(rob2(dp) , rob2(dp2));
+        
+    }
+    
+   int rob2(vector<int>& nums) {
+        vector<int> dp(nums.size(),0);
+        
+        dp[0] = nums[0];
+        if(nums.size() == 1)return nums[0];
+        
+        dp[1] = max(nums[0],nums[1]);
+        
+        for(int i =2 ; i < nums.size(); i++){
+            
+            dp[i] = max(nums[i] +dp[i-2] , dp[i-1] );
+            
+        }
+        return dp[nums.size()-1];
+    }
+      
+};
