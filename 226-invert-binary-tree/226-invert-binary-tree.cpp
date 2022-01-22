29297500c80a8bdd40d7c8ba2aa4462542b34bae
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-01-22 19:49:43 +0530

    Time: 5 ms (17.83%), Space: 9.8 MB (38.08%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -14,26 +14,12 @@ public:
     TreeNode* invertTree(TreeNode* root) {
         if(!root)return root;
         
-        if(!root->left and !root->right)
-            return root;
+        swap(root->left,root->right); 
         
-        dp(root);
+        invertTree(root->left);
+        invertTree(root->right);
         
         return root;
     }
     
-    void dp(TreeNode* r){
-        if(!r)return;
-        
-        // TreeNode* t = r->left;
-        // r->left = r->right;
-        // r->right = t;
-        
-        swap(r->left,r->right);
-        
-        dp(r->left);
-        dp(r->right);
-        
-
-    }
 };
