232edc72e3ee58b623eb016818da90c36fa3ef56
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-01-30 19:46:28 +0530

    Time: 37 ms (5.92%), Space: 20 MB (31.90%) - LeetHub

---
+++ 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<string> v;
+    string smallestFromLeaf(TreeNode* root) {
+      
+        dp(root,"");
+        
+        sort(v.begin(),v.end());
+        
+        return v[0];
+    }
+    
+    void dp(TreeNode* root, string s){
+        if(!root)return;
+        
+        s += char(root->val+'a');
+        
+        if(!root->left and !root->right){
+            reverse(s.begin(),s.end());
+            v.push_back(s);
+            return;
+        }
+        
+        dp(root->left, s);
+        dp(root->right,s);
+        
+        
+    }
+    
+    
+};
