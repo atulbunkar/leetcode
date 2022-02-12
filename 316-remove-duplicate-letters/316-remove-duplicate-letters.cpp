1fbfbc8670f245c8d2cef4246765686599f065cd
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-02-13 02:53:34 +0530

    Time: 3 ms (73.39%), Space: 6.6 MB (80.47%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> c(26,0);  string ans = "";
+        vector<int> done(26,0);
+        
+        for(auto x:s)
+            c[x-'a']++;
+        
+        for(auto x:s){
+            c[x-'a']--;
+            
+            if(done[x-'a']>0 )continue; done[x-'a']++;
+                
+            while(!ans.empty() and ans.back() > x and c[ans.back()-'a'] >0 ){
+         
+                done[ans.back()-'a']=0;
+                ans.pop_back();
+            }
+                
+            ans.push_back(x);
+        
+        
+        }
+        return ans;
+    }
+    
+    
+    
+    
+};
