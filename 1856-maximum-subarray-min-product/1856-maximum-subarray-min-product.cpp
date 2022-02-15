b611d98437aabc278d968a3b21713dc76e28fa2c
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-02-15 17:23:17 +0530

    Time: 252 ms (64.43%), Space: 80.4 MB (82.11%) - LeetHub

--- 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
@@ -1,45 +1,55 @@
 class Solution {
 public:
     int maxSumMinProduct(vector<int>& nums) {
-       int n = nums.size();
-        int left[n], right[n];
-        long long int sum[n];
-        sum[0] = nums[0];
-        for(int i=1;i<n;i++){
-            sum[i] = sum[i-1]+nums[i];
-        }
-        stack<pair<int, int> > s1, s2;
-        for (int i = 0; i < n; ++i) {
+        int n = nums.size();
+        
+        int left[n],right[n]; //range till which i is minimun
+        
+        stack<pair<int,int>> st,stt ;
+        
+        for(int i=0; i<n; i++){
             int cnt = 1;
-            while (!s1.empty() && (s1.top().first) > nums[i]) {
-                cnt += s1.top().second;
-                s1.pop();
+            while(!st.empty() and st.top().first > nums[i] ){
+                
+                cnt += st.top().second;
+                st.pop();
             }
-            s1.push({ nums[i], cnt });
+            st.push(make_pair(nums[i],cnt));
             left[i] = cnt;
-        }
-        for (int i = n - 1; i >= 0; --i) {
-            int cnt = 1;
-            while (!s2.empty() && (s2.top().first) >= nums[i]) {
-                cnt += s2.top().second;
-                s2.pop();
-            }
 
-            s2.push({ nums[i], cnt });
+        }
+        
+        for(int i=n-1; i>=0; i--){
+            int cnt = 1;
+            while(!stt.empty() and stt.top().first >= nums[i] ){
+                 cnt += stt.top().second;
+                 stt.pop();
+            }
+            stt.push(make_pair(nums[i],cnt));
             right[i] = cnt;
         }
-        long long int res =0;
-        for(int i=0;i<n;i++){
-            int a = i-left[i]+1;
-            int b = i+right[i]-1;
-            long long int subArraySum;
-            if(a>0)
-                subArraySum = sum[b]-sum[a-1];
-            else
-                subArraySum = sum[b];
-            res = max(res, subArraySum*nums[i]);
+        
+        long long ans = 0 ,temp; 
+        long long sum[n]; 
+        sum[0] = nums[0];
+        
+        for(int i = 1 ; i < n ; i++ ){
+            sum[i] = sum[i-1] + nums[i]; 
         }
-        return res%1000000007;
+        
+        for(int i=0;i <n ;i++){
+            int a = i - left[i] +1 ; 
+            int b = i + right[i] -1 ;
+            
+            if(a>0)
+                temp = sum[b] - sum[a-1] ;
+            else
+                temp = sum[b];
+            
+            ans = max(temp*nums[i],ans);
+        }
+
+        return ans%1000000007;
             
     }
 };
