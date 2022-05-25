7f8ec6f10a85b7ba4fde41bcbcd4b39bf43fe998
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-05-25 16:15:49 +0530

    Time: 650 ms (11.76%), Space: 164.2 MB (12.78%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ret;
+        vector<int> t;
+        bt(ret,t,k,n,1);
+        
+        return ret;
+    }
+    
+    void bt( vector<vector<int>>& ret , vector<int> t , int k,int n, int l){
+        
+        if(t.size()==k){
+            ret.push_back(t);
+            return;
+        }
+        
+        for(int i= l; i<=n; i++){
+            
+            t.push_back(i);
+            //cout<<l<< " " << i << " " << endl;
+            bt(ret,t,k,n,i+1);
+            // cout<<l<< " " << i << " " << endl;
+            t.pop_back();
+            
+        }
+        return;
+    }
+};
