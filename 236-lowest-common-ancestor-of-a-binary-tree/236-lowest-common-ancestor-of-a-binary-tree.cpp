24d91fec3bb4764e0fc34c36f0b250d650add134
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-03 00:29:45 +0530

    Time: 19 ms (69.44%), Space: 14.3 MB (59.40%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,31 +9,19 @@
  */
 class Solution {
 public:
-    TreeNode* ans = NULL;
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        bool l=false,r=false;
-        
-        if(!root || root==p || root==q)return root;
-        
-        if(find(root->left,p) and find(root->left,q))
-            return lowestCommonAncestor(root->left,p,q);
-        else if(find(root->right,p) and find(root->right,q)){
-            return lowestCommonAncestor(root->right,p,q);
-        }
-        
+   TreeNode * dfsTraverse(TreeNode * root, TreeNode * p , TreeNode * q)
+{
+    if( root == p || root == q || root == NULL)
         return root;
-        
-        
-        
-    }
-    
-    bool find(TreeNode* root, TreeNode* p){
-        
-        if(!root)return false;
-        
-        if(root==p)return true;
-        
-        return find(root->left,p) || find(root->right,p);
-        
-    }
+    TreeNode * parent1 = dfsTraverse(root->left, p, q);
+    TreeNode * parent2 = dfsTraverse(root->right, p, q);
+    if( parent1 && parent2)
+        return root;
+    else
+        return parent1 ? parent1:parent2;
+}
+TreeNode * lowestCommonAncestor(TreeNode * root, TreeNode * p, TreeNode * q)
+{
+    return dfsTraverse(root, p, q);
+}
 };
