50d80db3628c9fa5fbaef7083bcbf541cb1cbb35
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-09 03:25:30 +0530

    Time: 4 ms (85.78%), Space: 7.9 MB (74.05%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1);
         
-        for(int i=0; i< n+1 ;i++){
-            int c = 0;
-            int j= i;
-            while(j){
-           
-                if(j&1)
-                    c++;
-                j = j >> 1;
+        for(int i=0; i<=n;i++){
+            int c = 0,j=i;
+            
+            while(j>0){
+                if(j%2 != 0)c++;
+                j /= 2;
             }
             ans[i] = c;
+            
         }
+        
         return ans;
            
     }
