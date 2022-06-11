eb788cf22b12f0905316e70df5691d34b3053551
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-12 05:11:18 +0530

    Time: 20 ms (40.52%), Space: 7.5 MB (49.18%) - LeetHub

--- 457-circular-array-loop/457-circular-array-loop.cpp
+++ 457-circular-array-loop/457-circular-array-loop.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool circularArrayLoop(vector<int>& nums) {
         int n= nums.size(),len;
-        vector<int> done(n,0); bool sign, s2;
+        bool sign, s2;
         
         if(n==1)return false;
-            
         
         for(int i=0;i<n; i++){
             
             int j=i;
-            sign = nums[i]>0; len = 0;
+            sign = nums[i]>0; 
+            len = 0;
             
             while(len<n){
                 j = j + nums[j];
@@ -20,11 +20,11 @@ public:
                     j = n + (j%n);
                 
                 s2 = nums[j]>0;
-                if(sign!=s2){
-                    break;
-                }
                 
-                if(j==i and len ==0){
+                if(sign!=s2)
+                    break;
+                
+                if(j==i and len == 0){
                     break;
                 }
                 
