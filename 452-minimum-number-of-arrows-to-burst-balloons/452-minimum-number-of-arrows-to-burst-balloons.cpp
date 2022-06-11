2231725ec1dde71dc2533d9b6eb48634db30cba2
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-12 03:41:14 +0530

    Time: 455 ms (94.69%), Space: 90.1 MB (19.50%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -8,23 +8,24 @@ public:
         int ans =  p.size() , n = p.size();
         int s,e , curr_s,curr_e; bool got;
         
-        for(int i=0; i< n; i++){
+        v.push_back(make_pair(p[0][0],p[0][1]));
+        
+        for(int i=1; i< n; i++){
             curr_s = p[i][0] ; curr_e = p[i][1];
-            //cout<<endl;
             got = false;
-            
-            for(auto& x:v){
-                s = x.first; e = x.second;
-                //cout<< curr_s << " " << curr_e << " ";
-                //cout<< s << " " << e<< endl;
-                
-                if(curr_s==s || curr_e <= e || curr_s<= e){
-                    ans--; got = true;
-                    x.first = max(curr_s,s);
-                    x.second = min(curr_e,e);
-                    break;
-                }
+         
+            pair<int,int> x= v.back(); 
+      
+            s = x.first; e = x.second;
+       
+            if(curr_s==s || curr_e <= e || curr_s<= e){
+                ans--; got = true;
+                s = max(curr_s,s);
+                e = min(curr_e,e);
+                v.pop_back();
+                v.push_back(make_pair(s,e));
             }
+      
             if(!got)v.push_back(make_pair(curr_s,curr_e));
         }
         return ans;
