7a551ae7c68bb14c7fd986591484ab413521f958
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-13 03:06:20 +0530

    Time: 7 ms (94.92%), Space: 20.2 MB (64.80%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -3,18 +3,23 @@ class Solution {
 public:
     int ans = 0;
     int diameterOfBinaryTree(TreeNode* root) {
+        if(!root)return 0;
         help(root);
+       
         return ans;
     }
     
     int help(TreeNode* root){
         if(!root)return 0;
         
-        int l =  help(root->left) ;
-        int r = help(root->right);
+        if(!root->left and !root->right)return 1;
+ 
+        int x = help(root->left) ;
+        int y = help(root->right) ;
         
-        ans = max(ans , l+r);
-        
-        return  max( l+1 , r+1);
+        ans = max(x+y,ans);
+        return max(x,y)+1;
     }
+    
+    
 };
