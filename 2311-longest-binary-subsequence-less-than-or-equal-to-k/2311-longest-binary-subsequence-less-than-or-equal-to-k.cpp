50125ca3e8d3b99b551564a6fb0598180fac7d99
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-19 17:15:17 +0530

    Time: 319 ms (33.33%), Space: 265.1 MB (66.67%) - LeetHub

---
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestSubsequence(string s, int k) {
+        vector<vector<int>> mem(s.size()+1, vector<int>(s.size()+1,-1));
+         int n= s.size(); int ans=-1,p;
+        int t =0;
+        
+        for(int i=n-1;i>=0;i--){
+            p=0; t=0;
+            for(int j=i;j>=0;j--){         
+                if(s[j]-'0' == 1 ){
+                    if(p>30)continue;
+                    else t += pow(2,p);
+                }
+          
+                if(t>k){t-= pow(2,p); }
+                else p++;
+            } 
+            ans = max(ans,p);      
+        }   
+        return ans;
+    }
+
+};
