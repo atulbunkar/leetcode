93ca2342364c31f787a3d7169500d51d19ede445
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-20 20:21:27 +0530

    Time: 38 ms (21.24%), Space: 8.6 MB (33.38%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -3,22 +3,38 @@ public:
     vector<int> dp;
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size(); dp.resize(n+1,-1);
-        return dfs(0,arr,k);
-    }
-    int dfs(int i ,vector<int>& arr, int k){
-        if(i>=arr.size() )return 0; int ans = 0; int maxx = -1;
-        int nums=0;
         
-        if(dp[i]>-1)return dp[i];
+        dp[n] =0; int maxx;
         
-        for(int m=i; m< i+k ;m++){
-            if(m>=arr.size())break;
+        for(int i=n-1; i>=0;i--){
+            maxx= 0; 
+            for(int j=i;j<i+k ;j++){
+                if(j>=arr.size())break;
+                
+                maxx = max(maxx,arr[j]);
+                
+                dp[i] = max(dp[i], maxx*(j-i+1) + dp[j+1]) ;
+            }
             
-             maxx = (max(maxx,arr[m])); //store largest for curr window, winlen is changing 1 to k
-             nums++;                    //nums in curr window
-            
-            ans = max(ans, (nums)*maxx + dfs(m+1,arr,k) ); //max(ans, max(left part + right part))
         }
-        return dp[i] = ans;
+        return dp[0];
     }
+    
+//     //topdown
+//     int dfs(int i ,vector<int>& arr, int k){
+//         if(i>=arr.size() )return 0; int ans = 0; int maxx = -1;
+//         int nums=0;
+        
+//         if(dp[i]>-1)return dp[i]; //Do bottom up
+        
+//         for(int m=i; m< i+k ;m++){
+//             if(m>=arr.size())break;
+            
+//              maxx = (max(maxx,arr[m])); //store largest for curr window, winlen is changing 1 to k
+//              nums++;                    //nums in curr window
+            
+//             ans = max(ans, (nums)*maxx + dfs(m+1,arr,k) ); //max(ans, max(left part + right part))
+//         }
+//         return dp[i] = ans;
+//     }
 };
