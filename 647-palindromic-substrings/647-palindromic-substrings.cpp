03601002d41d5953be8875b17ef399190b12909f
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-22 23:19:48 +0530

    Time: 48 ms (23.03%), Space: 20.1 MB (21.61%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -4,12 +4,12 @@ public:
     int countSubstrings(string s) {
         ans = s.size(); int n=s.size();
         
-        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        vector<vector<int>> dp(n,vector<int>(n,-1));
         
         for(int i=0;i<n;i++)
             dp[i][i] = true;
         
-        for(int i=0; i<n;i++){
+        for(int i=0; i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 if(ispalin(s,i,j,dp))ans++;
             }
@@ -17,12 +17,12 @@ public:
         return ans;
     }
     
-    bool ispalin(string& s,int i,int j,vector<vector<bool>>& dp){
+    int ispalin(string& s,int i,int j,vector<vector<int>>& dp){
         
-        if(i >= j)return true;
-        if(dp[i][j])return true;
+        if(i >= j)return 1;
+        if(dp[i][j]>=1)return dp[i][j];
             
-        if(s[i] != s[j])return false;
+        if(s[i] != s[j])return 0;
         
         else
             return dp[i][j] = ispalin(s,i+1,j-1,dp);
