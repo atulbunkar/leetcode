0d15e6d9fd2c2a5dcecc2377a4f129e10cff5bff
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-30 00:32:00 +0530

    Time: 382 ms (5.50%), Space: 7.3 MB (50.31%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
+    int ret=0; 
     int numberOfArithmeticSlices(vector<int>& nums) {
         
-        int n=nums.size(); int ret=0,old=0,curr=0;
+        int n=nums.size(); int curr=0;
         
-        for(int i=1;i<n-1;i++){
-            
-            if(i<n-1 and nums[i]-nums[i-1] == nums[i+1]-nums[i]){
-                curr = old+1; 
-                
+        for(int i=0;i<n-2;i++){
+            for(int j=i+2;j<n;j++){
+                if(isarth(i,j,nums))curr++;
             }
-            ret += curr;
-            old = curr;
-            curr = 0;
-            
         }
-        return ret;
+        return curr;
     }
+   
+    
+    bool isarth(int i,int j,vector<int>& nums){
+        if(i==j)return true;
+        
+        if(nums[i+1]-nums[i] == nums[j]-nums[j-1])return isarth(i+1,j,nums) and isarth(i,j-1,nums);
+        
+        return false;
+        
+    }
+   
     
 };
