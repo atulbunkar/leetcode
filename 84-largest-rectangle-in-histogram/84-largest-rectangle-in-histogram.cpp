577a5d4dbaa6d29625149d8a18cdc5a874303b90
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-30 04:24:16 +0530

    Time: 367 ms (16.87%), Space: 81.5 MB (43.76%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,66 +1,51 @@
 class Solution {
 public:
-    void getleftshort(vector<int>& lft, vector<int>& h){
-        
-        stack<int> st; int n = h.size();
-        
-        for(int i= n-1; i >= 0 ; i--){
-            
-            while(!st.empty() and h[i] < h[st.top()]  ){
-                
-                lft[st.top()] = i+1;
-                st.pop();
-            }
-            st.push(i);
-        }
-        
-        while(!st.empty()){
-            lft[st.top()] = 0;
-            st.pop();
-        }
-        return;
-        
-    }
-    
-    void getrightshort(vector<int>& rht, vector<int>& h){
-        stack<int> st;  int n = h.size();
-        
-        for(int i= 0; i < h.size(); i++){
-            
-            while(!st.empty() and h[i] < h[st.top()]  ){
-                
-                rht[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        while(!st.empty()){
-            rht[st.top()] = n;
-            st.pop();
-        }
-        return;
-    }
-    
     int largestRectangleArea(vector<int>& h) {
-        int n = h.size();
-        vector<int> lft(n); //left range till h[i] is min
-        vector<int> rht(n); //right range till h[i] is min
-        
-        getleftshort(lft,h);   getrightshort(rht,h);
-        
+        int n= h.size();
+        vector<int> left(n),right(n);
+        getfromleft(h,left);
+         getfromright(h,right);
         
         int ans = 0;
         
-        for(int i=0; i< n;i++){
-           // cout<< lft[i] << " " << rht[i]<< endl;
-            int area = rht[i]-lft[i];
-            area = area*h[i];
-            
-            ans = max(ans,area); 
-            
+        for(int i=0;i<n;i++){
+            ans = max(ans , (h[i])*( right[i]-left[i] +1));
         }
-  
-        return ans;
         
+        
+        return ans;
+    }
+    
+    void getfromleft(vector<int>& h , vector<int>& left){
+         stack<int> st; int n= h.size();
+         st.push(0); left[0] = 0;
+        
+         for(int i=1;i<n;i++){
+             
+             while(!st.empty() and h[st.top()] >= h[i]){
+                 st.pop();
+             }
+             if(st.empty())left[i]=0;
+             else left[i] = st.top()+1;
+             
+             st.push(i);
+         }
+    
+    }
+    
+    void getfromright(vector<int>& h , vector<int>& right){
+        stack<int> st; int n= h.size();
+         st.push(n-1); right[n-1] = n-1;
+        
+         for(int i=n-2;i>=0;i--){
+             
+             while(!st.empty() and h[st.top()] >= h[i]){
+                 st.pop();
+             }
+             if(st.empty())right[i]=n-1;
+             else right[i] = st.top()-1;
+             
+             st.push(i);
+         }
     }
 };
