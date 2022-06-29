85f25edbe50b38b8306e52396679cb17598132b1
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-29 19:21:10 +0530

    Time: 50 ms (15.40%), Space: 9.7 MB (37.78%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -5,23 +5,23 @@ public:
         int n=p.size(); 
         memset(dp,-1,sizeof(dp));
         
-        int c =bt(p,0,n-1,1);
+        int c = bt(p,0,n-1,1);
         
-        return c>0;
+        return c>0 ? true :false;
         
     }
     
-    bool bt(vector<int>& p , int st, int end , int turn){
+    int bt(vector<int>& p , int st, int end , int turn){
         
         if(end==st)return p[st];
         
-        if(dp[st][end][turn] != -1)return dp[st][end][turn];
+       if(dp[st][end][turn] != -1)return dp[st][end][turn];
         
         if(turn){
             return dp[st][end][turn]= max( p[st]+ bt(p,st+1,end,0 ) , p[end] + bt(p,st,end-1,0));
         }
         else{
-            return dp[st][end][turn] = -1 * max( p[st]+bt(p,st+1,end,1) , p[end]+bt(p,st,end-1,1) );
+            return dp[st][end][turn] =  min( -p[st]+bt(p,st+1,end,1) , -p[end]+bt(p,st,end-1,1) );
         }
         
     }
