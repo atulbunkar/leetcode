f01c909022ea4855aa5fc5201b8691b857c97835
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-06-29 19:30:01 +0530

    Time: 23 ms (43.31%), Space: 8.7 MB (45.52%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int dp[501][501][2];
+    int dp[501][501];
     bool stoneGame(vector<int>& p) {
         int n=p.size(); 
         memset(dp,-1,sizeof(dp));
         
-        int c = bt(p,0,n-1,1);
+        int c = bt(p,0,n-1);
         
         return c>0 ? true :false;
         
     }
     
-    int bt(vector<int>& p , int st, int end , int turn){
+    int bt(vector<int>& p , int st, int end ){
         
         if(end==st)return p[st];
         
-       if(dp[st][end][turn] != -1)return dp[st][end][turn];
+       if(dp[st][end] != -1)return dp[st][end];
         
-        if(turn){
-            return dp[st][end][turn]= max( p[st]+ bt(p,st+1,end,0 ) , p[end] + bt(p,st,end-1,0));
-        }
-        else{
-            return dp[st][end][turn] =  min( -p[st]+bt(p,st+1,end,1) , -p[end]+bt(p,st,end-1,1) );
-        }
+       
+        return dp[st][end]= max( p[st]+ bt(p,st+1,end ) , p[end] + bt(p,st,end-1));
+  
         
     }
     
