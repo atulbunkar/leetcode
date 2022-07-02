48c4b9a0e49c363612c65f1750609530254ea101
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-07-02 21:52:52 +0530

    Time: 339 ms (47.23%), Space: 88.8 MB (44.13%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mem[100001][3][2];
+    int maxProfit(vector<int>& p) {
+        int n = p.size();  memset(mem,-1,sizeof(mem));
+        
+        return dp(0,2,1,p);
+    }
+    
+    int dp(int i, int k, int buy, vector<int>& p){
+        if(i==p.size() || k==0)return 0;
+        
+        if(mem[i][k][buy] !=-1)return mem[i][k][buy];
+        
+        if(buy){ // can buy
+            return mem[i][k][buy] =  max( dp(i+1,k,0,p)-p[i] , dp(i+1,k,1,p) );
+            
+        }
+        else{
+            return mem[i][k][buy] = max( dp(i+1, k-1, 1,p)+p[i] , dp(i+1,k,0,p) ) ;
+         
+        }
+        
+    }
+};
