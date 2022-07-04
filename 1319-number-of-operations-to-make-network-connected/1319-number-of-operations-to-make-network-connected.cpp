e4db043c66f28cef79ffdb8d2d11db200074f5e6
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-07-05 02:23:43 +0530

    Time: 103 ms (99.56%), Space: 37.2 MB (99.80%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int pr[100001];
+    //int sz[10001];
+    int cnt=0;
+    int set;
+    int find(int v){
+        if(pr[v] == -1)
+            return pr[v] = v;
+        if(pr[v] == v)
+            return pr[v] = v;
+        return pr[v] = find(pr[v]);
+    }
+    void un(int a,int b){
+        a = find(a);
+        b = find(b);
+        if(a == b){
+            cnt++;
+             return ;
+        } 
+        // if(sz[a]<sz[b])
+        //     swap(a,b);
+        
+        pr[b] = a;
+       // sz[a] = sz[a]+sz[b];
+        set--;
+        return;
+    }
+
+    int makeConnected(int n, vector<vector<int>>& con) {
+         memset(pr,-1,sizeof(pr));
+        // memset(sz,1,sizeof(sz));
+        set =n;
+         for(auto &x:con){
+            un(x[0],x[1]); 
+         }
+        int need =0;
+        for(int i =0;i<n;i++){
+            if(pr[i]==-1)
+                need++;
+        }
+        //cout<<set<<endl;
+        if(set-1>cnt)
+            return -1;
+        return set-1;
+    }
+};
