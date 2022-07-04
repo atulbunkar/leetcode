539b7377a6694c1bcd6619e0f078ed6e75d9fe5d
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-07-05 01:17:40 +0530

    Time: 1385 ms (19.99%), Space: 14.3 MB (59.50%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,78 +1,54 @@
 class Solution {
-	// To Create a edges from "word" to all other words with difference one
-    void createGraph(unordered_map<string, vector<string>>&G, vector<string>& wordList, string word, int i, int n)
-    {
-        for(int j=i; j<n; j++)
-        {
-            int set=0,ans=0;
-			
-			// Comparing alphanets.
-            for(int k=0; k<word.length(); k++)
-            {
-				// if one alphabet is different
-                if(!set && word[k]!=wordList[j][k]) {set=1, ans=1;}
-				// if more than one alphabet is different
-                else if(set && word[k]!=wordList[j][k]) {ans=0; break;}
-            }
-			// creating an undirected edge from node "word" to node "wordList[j]"
-            if(ans)
-            {
-                G[word].push_back(wordList[j]);
-                G[wordList[j]].push_back(word);
-            }
-        }  
-    }
-	
-	// Function with the shorted path of undirected graph
-    int shortestPath(unordered_map<string, vector<string>>&G, string source, string sink, vector<string>& wordList)
-    {
-		// ds to store dist from the source to the node
-        unordered_map<string,int>dist;
-		// initializing dist with max value
-        for(auto c:wordList) dist[c]=INT_MAX;
-		
-		// Queue to perform BFS
-        queue<string>Q;
-        Q.push(source);
-        dist[source]=0;
+public:
+    int ladderLength(string bw, string ew, vector<string>& wl) {
+        wl.push_back(bw);
+        int n=wl.size(); 
+        if(find(wl.begin(),wl.end(), ew) == wl.end())return 0;
         
-        while(!Q.empty())
-        {
-            string s = Q.front();
-            Q.pop();
-            for(auto u:G[s])
-            {
-                if(dist[u]>dist[s]+1)
-                {
-                    dist[u]=dist[s]+1;
-                    Q.push(u);
+        vector<int> adj[5001];
+        
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(check(wl[i],wl[j])){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
-        return dist[sink];
+        
+        queue<int> q; 
+        vector<bool> vstd(n+1,false);
+        q.push(n-1); int step =1;
+        
+        while(!q.empty()){
+            int m = q.size();
+           // bool flag = false;
+            for(int j=0;j<m;j++){
+                
+                int curr = q.front(); q.pop();
+                
+                if(wl[curr]==ew)return step;
+                
+                //if(adj[curr].size() >0)flag =true;
+                
+                for(auto &i:adj[curr]){
+                    if(vstd[i] || wl[i] == bw)continue;
+                       // cout<< curr<< " "<< wl[i] << " "<<endl;
+                    vstd[i] = true;
+                    q.push(i);
+                }
+            }
+           // if(!flag)return 0;
+            step++;
+        }
+        return 0;
     }
-public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        bool isPresent=0;
-        int n=wordList.size();
-        
-		// checking if endWord is not present in the wordList
-        for(auto it:wordList) if(it==endWord) isPresent=1;
-        if(!isPresent) return 0;
-        
-		// Map is used bcuz we need string to access the index
-        unordered_map<string, vector<string>>G;
-		
-		// Adding edge from node "beginWord" to all other words of the wordList that contain one different alphabet
-        createGraph(G,wordList, beginWord, 0, n);
-        
-        for(int i=0; i<n; i++)
-            createGraph(G,wordList, wordList[i], i+1, n);
-        
-        int a = shortestPath(G, beginWord, endWord, wordList);
-		
-		// if there is no path from beginWord to endWord 
-        if(a==INT_MAX) a=-1;
-        return a+1;
+    
+    
+    bool check(string& i, string& j){
+        if(i.size() != j.size())return 0;
+         int c =0;
+        for(int k=0 ; k<i.size();k++)if(i[k] != j[k])c++;
+        return c==1;
     }
 };
