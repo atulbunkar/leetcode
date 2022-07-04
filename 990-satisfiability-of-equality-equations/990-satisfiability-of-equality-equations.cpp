4c4693cfce9642a021cf3e2a43f56f03bd05a460
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-07-05 01:50:52 +0530

    Time: 16 ms (17.97%), Space: 11.2 MB (90.40%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int pr[500];
+    int sz[500];
+    int find(int v){
+        if(pr[v] == -1)
+            return pr[v] = v;
+        if(pr[v] == v)
+            return pr[v] = v;
+        return pr[v] = find(pr[v]);
+    }
+    void un(int a,int b){
+        a = find(a);
+        b = find(b);
+        if(a == b)
+            return ;
+        if(sz[a]<sz[b])
+            swap(a,b);
+        pr[b] = a;
+        sz[a] = sz[a]+sz[b];
+        return;
+    }
+    
+    bool equationsPossible(vector<string>& eq) {
+        memset(pr,-1,sizeof(pr));
+        memset(sz,1,sizeof(sz));
+        for(auto &x:eq){
+            if(x[1] == '='){
+                un(x[0]-'a',x[3]-'a');
+            }
+            
+        }
+        for(auto &x:eq){
+             if(x[1] == '!'){
+                    int l = find(x[0]-'a');
+                    int r = find(x[3]- 'a');
+                    if(l==r)
+                        return 0;
+                }
+            
+        }
+            
+        return 1;
+    }
+};
