709ce7651e2d9925e25892d0487fbfa3dac23114
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-07-06 03:28:55 +0530

    Added solution - LeetHub

---
+++ Strongly Connected Components (Kosaraju's Algo) - GFG/strongly-connected-components-kosarajus-algo.cpp
@@ -0,0 +1,94 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+	public:
+	stack<int> s;  int Scc=0;
+	//Function to find number of strongly connected components in the graph.
+    int kosaraju(int V, vector<int> adj[])
+    {
+        vector<int> onstack(V+1,false);
+        vector<int> stime(V+1,-1);
+        vector<int> low(V+1,-1);
+    
+        int t =0;
+        for(int i=0;i<V;i++){
+            if(stime[i] == -1){
+                tarjan(i, adj,onstack,stime,low, t);
+            }
+        }
+       
+        return Scc;
+    }
+    
+    void tarjan(int i,vector<int> adj[] , vector<int>& onstack ,vector<int>& stime ,vector<int>& low,int& t){
+        onstack[i] = true;
+        stime[i] = low[i] = t++;  s.push(i);
+        
+        for(auto x:adj[i]){
+            
+            if(stime[x]!=-1 and onstack[x]){
+                low[i] = min(low[i], stime[x]);
+            }
+            
+            else if(stime[x]==-1){
+                tarjan(x,adj,onstack,stime,low,t);
+                
+                low[i] = min(low[i], low[x]);
+            }
+            
+        }
+   
+        if(low[i] == stime[i]){
+            Scc++;
+            while(s.top() != i){
+                onstack[s.top()] = false;
+                s.pop();
+            }
+            onstack[s.top()] = false;
+            s.pop();
+        }
+        
+        
+        
+        
+        
+    }
+    
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E;
+    	cin >> V >> E;
+
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.kosaraju(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
