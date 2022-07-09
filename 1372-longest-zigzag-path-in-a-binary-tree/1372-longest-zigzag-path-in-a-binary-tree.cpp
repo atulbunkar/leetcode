977d3a5229ac0e3691ab98166fbdcd83cc7f78e8
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-07-10 01:14:09 +0530

    Time: 1191 ms (5.05%), Space: 193.2 MB (5.13%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -1,7 +1,7 @@
 
 class Solution {
 public:
-    map<TreeNode* ,map<int,int>> mem;
+    map<pair<TreeNode*,int>, int > mem;
     
     int longestZigZag(TreeNode* root) {
         return help(root);
@@ -13,15 +13,15 @@ public:
     }
     
     int rec(TreeNode* r, int left){
-        if(mem.find(r) != mem.end() and mem[r].find(left) != mem[r].end() )return mem[r][left];
+        if( mem.find({r,left}) != mem.end() )return mem[{r,left}];
         
         if(!r)return -1;
         
         if(left){
-            return mem[r][left] =  1+rec(r->left,0);
+            return  mem[{r,left}] =  1+rec(r->left,0);
         }
         else{
-            return mem[r][left]= 1+rec(r->right,1);
+            return  mem[{r,left}]= 1+rec(r->right,1);
         }
     }
 };
