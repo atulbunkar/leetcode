456f96f6d9576ec9d29d36ddb85e571b5cda0bab
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-07-14 02:33:04 +0530

    Added solution - LeetHub

---
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template in C++
+
+class Solution 
+{
+    public:
+    //Function to find minimum number of pages.
+    int findPages(int nums[], int N, int M) 
+    {
+        int min=0,max=0;
+        for(int i=0;i<N;i++)max += nums[i];
+        int ans = -1;
+        
+        if(M>=N)return nums[N-1];
+        
+        while(min<=max){
+            int mid = min+ (max-min)/2;
+            int run =0; int k=M; bool f = false; int p=0;
+           // cout<< mid << "-> ";
+            for(int i=0;i<N;i++){
+                
+                if(run+nums[i] > mid ){
+                    if(k==1 || nums[i]>mid){f = true;break;}
+                    else k--;
+                    run = nums[i];
+                }
+                else
+                    run += nums[i];
+            }    
+            //cout << f<< " ";
+            if(f){
+                min = mid+1;
+            }
+            else {
+                ans = mid;
+                max = mid-1; 
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int A[n];
+        for(int i=0;i<n;i++){
+            cin>>A[i];
+        }
+        int m;
+        cin>>m;
+        Solution ob;
+        cout << ob.findPages(A, n, m) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
