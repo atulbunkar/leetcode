e03087e1c2cd82fc0bcb057ce1d7cda3d5d2c3d8
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-07-15 21:22:39 +0530

    Time: 18 ms (77.34%), Space: 16.4 MB (19.25%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+         string ans = ""; int len = INT_MAX-1;
+        int n = s.size(); int m = t.size();
+       
+        vector<int> mp(128); int cnt = m;
+ 
+        for(auto x:t)mp[x-'A']++;
+        int j=0,i=0;
+        
+        while(i<n){ 
+            if( mp[s[i] -'A']-- >0 ){
+                cnt--;
+             
+            }      
+            
+            while(cnt==0){
+                if(i-j < len){
+                    len = i-j;
+                    ans = s.substr(j,i-j+1);
+                }
+                
+                if(mp[s[j]-'A']++ ==0){
+                    cnt++;
+                }
+                j++;
+            }
+            i++;
+        }
+  
+        return len==INT_MAX-1 ? "":ans ;
+    }
+};
