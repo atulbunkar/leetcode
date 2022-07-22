7bc9e27dae50046d8f930bf724c56ec81b20d4fb
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-07-22 06:40:09 +0530

    Time: 102 ms (7.29%), Space: 20 MB (9.96%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.cpp
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& f, vector<vector<int>>& s) {
-        vector<vector<int>> ans; vector<int> t(2);
+        //make to 2 list in 1 and sort it by start pos
+        vector<vector<int>> ans;
+        for(auto &x :s)
+            f.push_back(x);
         
-        int i=0, j=0; int n= f.size(), m= s.size();
-        
-        if(!n || !m)return {};
-        
-        while(i<n and j<m){
-            
-            while(1){
-            
-                while(i<n and j<m and f[i][1] < s[j][0] )i++;
+        sort(f.begin(),f.end());
+        int n =f.size();
+        for(int i=0;i<n;i++){
+            int st = f[i][0], end = f[i][1];
+            int j=i+1;
+            while(j<n and f[j][0] <= end){
 
-                while(j<m and i<n and s[j][1] < f[i][0])j++;
-                
-                if(i<n and j<m and (f[i][1] < s[j][0] || s[j][1] < f[i][0] ) )continue;
-                else break;
-            }
-            
-            if( i>=n || j>=m )break;
-            
-            if(f[i][0] <= s[j][0])t[0] = s[j][0];
-            else t[0] = f[i][0];
-            
-            if(f[i][1] <= s[j][1])t[1] = f[i][1];
-            else t[1] = s[j][1];
-                
-            ans.push_back(t);
-            
-            if(i<n-1 and j<m and f[i+1][0] <= s[j][1] )i++;
-            
-            else if(i<n and j<m-1 and s[j+1][0] <=f[i][1])j++;
-            
-            else{
-                i++; j++;
+                int e = min(end,f[j][1]) ;
+                ans.push_back({f[j][0],e});
+                j++;
             }
         }
-        //cout<< i << " "<< j;
         
-        return ans;
+         return ans;
+        
     }
 };
