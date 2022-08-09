b0ca850c9675cbce7b762b7ed6955c1e062a740b
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-08-10 02:01:18 +0530

    Time: 654 ms (79.94%), Space: 140.4 MB (68.06%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     long long interchangeableRectangles(vector<vector<int>>& r) {
-        map<double,long long> mp;
+        unordered_map<double,long long> mp;
         
-        for(auto x:r){
+        for(auto& x:r){
             double k = x[0]*1.0/x[1];
             mp[k]++;
         }
         
         long long ans=0;
-        for(auto x:mp){
+        for(auto& x:mp){
             long n = x.second;
             ans += n*(n-1)/2;
         }
