f99760b8def45e5a19c213fd5cd68fceb10ef6ce
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-08-27 01:45:49 +0530

    Time: 199 ms (65.89%), Space: 93.1 MB (96.05%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -9,7 +9,13 @@ public:
             if(p[i] != p[i-1]-1)j = i;
             
             ans += i-j+1;
-            
+            // this adds the new number to all prev sets. 
+            // say [3,2,1]
+            //currently i at idx 2 and j at idx 0
+            //prev sets were : {3,2} , {2}.
+            //so , we do 2-0+1 = 3 , i,e 3 new sets are got
+            //which are {3,2,1} , {2,1} ; {1}
+            //even if i==j , one element set is got.
         }
         
         return ans;
