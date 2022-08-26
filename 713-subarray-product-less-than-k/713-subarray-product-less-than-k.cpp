36f9bf3175938f884899eb22fad666d14c0f908e
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-08-27 01:22:47 +0530

    Time: 96 ms (86.89%), Space: 61.4 MB (20.53%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-     
+        // atmost k-1;
+        k--;
+        
         int n = nums.size();
-        if(k==0)return 0;
         
-        //DO USING SLIDING WINDOW : 
-        // recall how to add all windows btw i to j;
+        int ans = 0;
+        long p = 1 ,j=0;
         
-        long long p=1; int ans =0,j=0;
-
         for(int i=0;i<n;i++){
             
             p *= nums[i];
             
-            while(j<i and p>=k){
+            while(j<i and p>k){
                 p /= nums[j++];
             }
             
-            if(p<k)
-                ans += i-j+1;
-           
+            if(p<=k)ans+= i-j+1 ;
+            
         }
+
         
         return ans;
         
