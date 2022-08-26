d04c11e8b1eeac3143f599661c3ab6ba98c01fd3
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-08-26 18:56:43 +0530

    Time: 723 ms (22.60%), Space: 204.1 MB (20.00%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,41 @@
+
+class Solution {
+public:
+    
+    unordered_map<int,int> odds; //ele , count
+   // int odd[100001]
+        int ans=0;
+        
+    int pseudoPalindromicPaths (TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+    
+    
+    void dfs(TreeNode* r){
+        
+        if(!r)return;
+        
+        odds[r->val]++;
+        
+         if(!r->left and !r->right){
+           
+             int o = 0;
+             for(auto x:odds){
+                 if(x.second%2)o++;
+             }
+             if(o<=1)ans++;
+           
+        }
+        
+        dfs(r->left);
+        
+        dfs(r->right);
+        
+        odds[r->val]--;
+        if(odds[r->val]<=0)odds.erase(r->val);
+        
+    }
+    
+    
+};
