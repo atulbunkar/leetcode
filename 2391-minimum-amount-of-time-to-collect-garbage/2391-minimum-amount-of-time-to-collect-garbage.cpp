49fad8b64c27690bfb95febc694d5f272d55d43f
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-08-29 20:15:19 +0530

    Time: 1658 ms (10.00%), Space: 278.2 MB (30.00%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& ga, vector<int>& t) {
+        int ans = 0;
+        
+        vector<char> kk = {'P', 'G', 'M'};  
+        
+        int n = ga.size();
+        vector<map<char,int>> v;
+        
+        for(auto x:ga){
+            map<char,int> mp;
+            for(auto c:x){
+                mp[c]++;
+            }
+            v.push_back(mp);
+        }
+        
+        for(auto x:kk){
+            int tmp =0; 
+            
+            for(int i=0;i<n;i++){
+                
+                if(v[i].count(x)){
+                   // cout<< i << " ";
+                    ans += tmp;
+                    ans += v[i][x];
+                    tmp =0;
+                }
+                if(i<n-1)tmp += t[i];
+            }
+           // cout<< ans << " ";
+        }
+        
+        return ans;
+    }
+};
