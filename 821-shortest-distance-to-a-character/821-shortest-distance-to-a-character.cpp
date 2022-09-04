510f49d47aa7b1e3ef037c2494cb6fc4cd0e18ec
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-09-05 01:17:24 +0530

    Time: 6 ms (41.51%), Space: 6.7 MB (84.29%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
-        vector<int> cc;
+       
+        int n = s.size();
         
-        int idx=0;
+        vector<int> ans(n,INT_MAX);
+        int cnt = 0;
+        int  i=0;
         
-        for(auto& x:s){
-            if(x==c)cc.push_back(idx);
-            idx++;
+        while(i<n)if(s[i++]==c)break; i--;
+        
+        for(;i<n;i++){
+            if(s[i]==c)cnt = 0;
+            ans[i] = cnt++;
         }
         
-        vector<int> ans;
+        cnt = 0; int j=n-1;
+        while(j>=0)if(s[j--]==c)break; j++;
         
-        for(int i=0;i<s.size() ;i++){
-            
-            if(s[i]==c){ans.push_back(0); continue;}
-            
-            auto x = lower_bound(cc.begin(),cc.end(), i);
-            
-            if(x == cc.begin()  ){
-                ans.push_back(abs(*x - i) );
-                continue;
-            }
-            if(x== cc.end()){
-                ans.push_back(abs(cc[cc.size()-1] - i) );
-                continue;
-            }
-          
-            auto y = x-1;
-            int pus = min(abs(*x - i) , abs(*y - i));
-            ans.push_back(pus );
-            
+        for(;j>=0;j--){
+            if(s[j]==c)cnt = 0;
+            ans[j] = min(ans[j], cnt++);
         }
+        
         return ans;
     }
 };
