2d61dd4a2fcc5dd6b110bce52bd93f551b696e4f
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-09-13 15:30:48 +0530

    Time: 30 ms (44.65%), Space: 8.3 MB (93.07%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    vector<int> dp;
+    int mem[501];
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        int n = arr.size(); dp.resize(n+1,-1);
+        memset(mem,-1,sizeof(mem));
+        return dp(0,arr,k);
         
-        dp[n] =0; int maxx;
-        
-        for(int i=n-1; i>=0;i--){
-            maxx= 0; 
-            for(int j=i;j<i+k ;j++){
-                if(j>=arr.size())break;
-                
-                maxx = max(maxx,arr[j]);
-                
-                dp[i] = max(dp[i], maxx*(j-i+1) + dp[j+1]) ;
-            }
-            
-        }
-        return dp[0];
     }
     
-//     //topdown
-//     int dfs(int i ,vector<int>& arr, int k){
-//         if(i>=arr.size() )return 0; int ans = 0; int maxx = -1;
-//         int nums=0;
+    int dp(int i, vector<int>& arr, int k ){
+     
+        if(i>=arr.size())return 0; 
         
-//         if(dp[i]>-1)return dp[i]; //Do bottom up
+        if(mem[i]>-1)return mem[i];
         
-//         for(int m=i; m< i+k ;m++){
-//             if(m>=arr.size())break;
+        int mx = arr[i];
+        int ans = -1;
+        
+        for(int s = i; s<i+k and s< arr.size() ; s++){
             
-//              maxx = (max(maxx,arr[m])); //store largest for curr window, winlen is changing 1 to k
-//              nums++;                    //nums in curr window
+            mx = max(mx,arr[s]);
             
-//             ans = max(ans, (nums)*maxx + dfs(m+1,arr,k) ); //max(ans, max(left part + right part))
-//         }
-//         return dp[i] = ans;
-//     }
+            ans = max(ans , mx*(s-i+1) + dp(s+1,arr,k) );
+            
+        }
+        
+        return mem[i] = ans;
+        
+        
+    }
 };
