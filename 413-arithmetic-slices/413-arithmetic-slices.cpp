6b90d2a04ed535f6a183e2aec937b81f331e0aff
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-09-29 17:24:20 +0530

    Time: 3 ms (68.70%), Space: 7.4 MB (13.12%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    int ret=0;  vector<vector<int>> mem;
-    
     int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n);
         
-        int n=nums.size(); int curr=0;
-         mem.resize(n+1,vector<int>(n+1,-1));
+        int ans = 0;
         
-        for(int i=0;i<n-2;i++){
-            for(int j=i+2;j<n;j++){
-                if(isarth(i,j,nums))curr++;
+        for(int i=2;i<n;i++){
+            
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]){
+                dp[i] = dp[i-1]+1;
             }
+            else
+                ans += (dp[i-1]*(dp[i-1]+1))/2;
+            
         }
-        return curr;
+        
+        ans += dp[n-1]*(dp[n-1]+1)/2;
+        return ans;
+        
+        
+        
     }
-   
     
-    bool isarth(int i,int j,vector<int>& nums){
-        if(i==j)return true;
-        
-        if(mem[i][j] != -1)return mem[i][j];
-        
-        if(nums[i+1]-nums[i] == nums[j]-nums[j-1])return mem[i][j]= isarth(i+1,j,nums) and isarth(i,j-1,nums);
-        
-        return mem[i][j] = false;
-        
-    }
-   
+    
     
 };
