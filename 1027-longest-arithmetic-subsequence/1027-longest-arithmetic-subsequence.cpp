16c896deab1390a10aa917f99546c30ef31661f6
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-09-30 15:34:15 +0530

    Time: 388 ms (72.59%), Space: 18.3 MB (84.52%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -5,19 +5,20 @@ public:
         int n =nums.size(); int ans=1;
         //string k, kk;
         int mp[1001][2002] ;
-        memset(mp,-1,sizeof(mp));
-        
-        
-        for(int i=0;i<n;i++){
+        for(int i = 0 ; i < 1001 ; i++){
+            for(int j = 0 ; j < 2002 ; j++){
+            mp[i][j] = 1;
+        }
+        }
+        for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 
 //                 k = to_string(j) + "," + to_string(nums[i]-nums[j]);
 //                 kk = to_string(i) + "," + to_string(nums[i]-nums[j]);
                 int d = nums[i]-nums[j];
                 d += 1000;
-                
-                if(mp[j][d] >-1)mp[i][d] = mp[j][d]+1;
-                else mp[i][d] = 2;
+                mp[i][d] = mp[j][d]+1;
+                //else mp[i][d] = 2;
                 
                 ans = max(ans, mp[i][d]);
                
