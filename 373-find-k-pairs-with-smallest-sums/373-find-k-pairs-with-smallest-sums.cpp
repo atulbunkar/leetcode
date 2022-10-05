710abc55cd12bac299f2f3f91bff5f78b70a943c
Atul Bunkar <atul.bunkar04@yahoo.com>
2022-10-05 18:48:40 +0530

    Time: 590 ms (42.28%), Space: 119.3 MB (32.48%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int i=0,j=0;  vector<vector<int>> ans;
-        int n1 =nums1.size() , n2= nums2.size(); int k1=0,l=0;
-        
-        multimap<int , pair<int,int>> mp;
-        
-        for(int i = 0;i< n1; i++)
-            for(j = 0; j<n2; j++){
-                int s = nums1[i]+nums2[j];
-               
-                if(mp.size()<k)
-                    mp.insert(make_pair(s,make_pair(nums1[i],nums2[j]))) ;
-                else if(s < mp.rbegin()->first){
-                    mp.erase(prev(mp.end()));
-                    mp.insert(make_pair(s,make_pair(nums1[i],nums2[j]))) ;
-                }
-                else
-                    break;
-            }
-        
-        for(auto it = mp.begin() ; it != mp.end() ; it++){
-            if(k-- == 0)break;
-            auto x = it->second;
-            
-            ans.push_back({x.first, x.second});
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>> pq;
+        for(int i = 0 ; i < nums1.size(); i++){
+            pq.push({nums1[i]+nums2[0],{i,0}});
         }
-        
-        
+        int n = nums2.size();
+        vector<vector<int>> ans;
+        while(k-- && !pq.empty()){
+            auto x = pq.top();
+            pq.pop();
+            int i = x.second.first;
+            int j = x.second.second;
+            ans.push_back({nums1[i],nums2[j]});
+            j++;
+            if(j < n)
+                pq.push({nums1[i]+nums2[j],{i,j}});
+            
+        }
         return ans;
     }
 };
